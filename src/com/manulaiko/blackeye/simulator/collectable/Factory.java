352c3f00996cea8b845fcb62f38f9e98f9e99072
Manulaiko <manulaiko@gmail.com>
2016-10-20 16:14:05 +0200

    Created collectable factory

---
+++ src/com/manulaiko/blackeye/simulator/collectable/Factory.java
@@ -0,0 +1,33 @@
+package com.manulaiko.blackeye.simulator.collectable;
+
+import java.sql.ResultSet;
+
+/**
+ * Factory for the `collectables` table.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class Factory extends com.manulaiko.blackeye.simulator.Factory
+{
+    /**
+     * Constructor.
+     */
+    public Factory()
+    {
+        super("collectables", false);
+    }
+
+    /**
+     * Builds and returns a collectable.
+     *
+     * @param rs Query result.
+     *
+     * @return Collectable object.
+     */
+    public Object build(ResultSet rs)
+    {
+        Builder b = new Builder(rs);
+
+        return b.build();
+    }
+}
