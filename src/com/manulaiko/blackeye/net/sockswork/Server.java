62e270541c01fa6e0a91646e2a5e672182feb865
Manulaiko <manulaiko@gmail.com>
2016-10-23 18:44:58 +0200

    Created 'net.sockswork'

---
+++ src/com/manulaiko/blackeye/net/sockswork/Server.java
@@ -0,0 +1,74 @@
+package com.manulaiko.blackeye.net.sockswork;
+
+import java.net.Socket;
+
+import com.manulaiko.blackeye.launcher.Main;
+import com.manulaiko.tabitha.Console;
+
+/**
+ * Chat server.
+ *
+ * Starts chat server on port 4301.
+ *
+ * @author Manulaiko <manulaiko@gmail.com>
+ */
+public class Server extends com.manulaiko.tabitha.net.Server
+{
+    /**
+     * Constructor.
+     */
+    public Server()
+    {
+        super(Main.configuration.getShort("core.sockswork_port"));
+    }
+
+    /**
+     * Waits for connections and handles them.
+     */
+    public void onRunning()
+    {
+        try {
+            Socket socket = this.acceptConnection();
+            this.addConnection(new Connection(socket));
+
+            Console.println("Received connection from "+ socket.getInetAddress().getHostAddress());
+        } catch(Exception e) {
+            //Empty
+        }
+    }
+
+    /**
+     * Stops the server.
+     *
+     * @param timeout Timeout to wait before stopping the server.
+     */
+    public void stop(int timeout)
+    {
+        try {
+            // TODO Timeout
+            super.stop();
+        } catch(Exception e) {
+            //Empty
+        }
+    }
+
+    /**
+     * Finishes a connection.
+     *
+     * @param id Connection ID.
+     */
+    public void finishConnection(int id)
+    {
+        if(!this._connections.containsKey(id)) {
+            return;
+        }
+
+        com.manulaiko.tabitha.net.Connection c = this._connections.get(id);
+
+        c.close();
+
+        c = null;
+
+        this._connections.remove(id);
+    }
+}
