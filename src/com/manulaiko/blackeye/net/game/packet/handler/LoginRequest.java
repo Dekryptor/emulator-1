2fb336396aa32500135510dee3cd0f8d196dcbe1
Manulaiko <manulaiko@gmail.com>
2016-11-09 00:27:12 +0100

    Deleted debug message

--- src/com/manulaiko/blackeye/net/game/packet/handler/LoginRequest.java
+++ src/com/manulaiko/blackeye/net/game/packet/handler/LoginRequest.java
@@ -22,8 +22,6 @@ public class LoginRequest extends Packet
      */
     public void handle(Connection connection)
     {
-        Console.println("Hey... don't ignore my breakpoint :/");
-
         int id           = this._packet.readInt();
         String sessionID = this._packet.readString();
         Account account  = this._loadAccount(id, sessionID);
